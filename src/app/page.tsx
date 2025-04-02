206e8333b9e64a72bb5d10a76002883f6dd33ce0
Guillermo/Muñoz <113558613+Guillermo-Munoz@users.noreply.github.com>
2025-04-02 12:42:18 +0200

    modificar bienvenida

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,8 @@
 export default function Home() {
   return (
     
-      <h1 className="flex items-center bg-amber-500 py-5 justify-center">Home</h1>
+      <h1 className="flex items-center bg-amber-500 py-100 justify-center text-8xl">Bienvenido a Localiten.</h1>
+     
 
     
   );
