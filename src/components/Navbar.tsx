ead61d2c7fc275d373bfce51cdaccc84a76b7c9c
Guillermo/Muñoz <113558613+Guillermo-Munoz@users.noreply.github.com>
2025-04-02 12:46:10 +0200

    modificar aspectos visuales de barra de navegación

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,52 +1,46 @@
 "use client"
 import Link from "next/link"
-import {signIn, signOut, useSession} from "next-auth/react"
+import { signIn, signOut, useSession } from "next-auth/react"
+
 /**
  * Navbar: Componente de navegación principal de la aplicación.
  * Muestra enlaces de navegación y botones de autenticación basados en el estado de la sesión del usuario.
  *
  * @returns {JSX.Element} El componente Navbar.
- */ // Documentación JSDoc
- 
-function Navbar(){
-  
-  const { data: session} = useSession()
-  console.log(session)
-  
+ */
 
-  return(
- 
-    <nav className="bg-gray-800 text-white p-4 flex justify-between items-center">
-      <Link href="/">
-       <h1>
-        Localiten
-       </h1>
+function Navbar() {
+  const { data: session } = useSession()
+
+  return (
+    <nav className="bg-gray-800 text-white p-4 flex justify-between items-center text-3xl">
+      <Link href="/" className="font-bold">
+        Localiten<span className="text-5xl text-amber-400">.</span>
       </Link>
 
-      {session?.user ? (//se comprueba si esta logueado para mostrar el boton de singIn o singOut
-        <div className="flex gap-x-2 items-center">
-          <Link href="/dashboard">
-          dashboard
+      {session?.user ? ( // Si el usuario está autenticado
+        <div className="flex gap-x-4 items-center">
+          <Link href="/dashboard" className="text-lg hover:text-amber-400 transition">
+            Dashboard
           </Link>
-          <p>{session.user.name} {session.user.email}</p>
-                                                                            
-        <button onClick={() => signOut()} className="bg-red-400 px-3 py-2"> 
-          Sing Out
-        </button>
+          <p className="text-lg">{session.user.name || "Usuario"}</p>
+          <button 
+            onClick={() => signOut()} 
+            className="bg-red-500 px-4 py-2 rounded-lg hover:bg-red-600 transition"
+          >
+            Sign Out
+          </button>
         </div>
-      ): (
-        <div className="flex gap-x-2 items-center">
-        <Link href="/dashboard">
-        dashboard
-        </Link>
-                                                                           
-      <button onClick={() => signIn()} className="bg-sky-400 px-3 py-2"> 
-        Sing In
-      </button>
-      </div>
-
+      ) : ( // Si NO está autenticado
+        <button 
+          onClick={() => signIn()} 
+          className="bg-amber-400 px-4 py-2 rounded-lg hover:bg-amber-500 transition"
+        >
+          Sign In
+        </button>
       )}
     </nav>
   )
 }
+
 export default Navbar;
